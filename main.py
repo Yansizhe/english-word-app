d923b34a24dd6d78c99cd54e2442e9fdf887f49b
Yansizhe <3584405410@qq.com>
2026-05-13 21:02:46 +0800

    添加循环

--- main.py
+++ main.py
@@ -3,27 +3,35 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
-# 第2段：加载 .env 并取出 Key
+# 加载 .env 并取出 Key
 load_dotenv()   # 作用：找到当前文件夹下的 .env 文件，把里面写的变量加载到环境变量里
 
-api_key = os.getenv("DEEPSEEK_API_KEY")
+api_key = os.getenv("DEEPSEEK_API_KEY")  # 使用放在环境变量里的关键变量
 
 
-# 第3段：连接 DeepSeek，发请求
+# 连接 DeepSeek，发请求
 client = OpenAI(
     api_key=api_key,
     base_url="https://api.deepseek.com"
 )
 
-user_input = input("请输入要学习的英文单词:")
 
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role":"system","content":"你是一位英语老师。对每个单词给出：中文释义、词性、英文例句、例句中文翻译。"},
-        {"role":"user","content":f"请讲解单词：{user_input}"}
-    ]
-)
+#添加循环
+while True: 
+    user_input = input("请输入要学习的英文单词:")
 
-# 第4段：打印 AI 的回答
-print("AI 回答：", response.choices[0].message.content)
+    if user_input.lower() == "quit":
+        break
+    # 另一种写法：
+    # if user_input.lower() in ["quit","exit","q"]:
+    #   break
+    
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role":"system","content":"你是一位英语老师。对每个单词给出：中文释义、词性、英文例句、例句中文翻译。"},
+            {"role":"user","content":f"请讲解单词：{user_input}"}
+        ]
+    )
+    # 打印 AI 的回答
+    print("AI 回答：", response.choices[0].message.content)
