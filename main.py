9c247d3c52ff562726a36e9f041c3764c17242aa
Yansizhe <3584405410@qq.com>
2026-05-13 21:17:16 +0800

    回复无效单词

--- main.py
+++ main.py
@@ -27,14 +27,17 @@ while True:
     #   break
     
     if not (user_input.isascii() and user_input.isalpha()):
-        print("请输入正确的形式！")
+        print("请输入英语单词！")
         print(end="\n")
         continue
 
     response = client.chat.completions.create(
         model="deepseek-chat",
         messages=[
-            {"role":"system","content":"你是一位英语老师。对每个单词给出：中文释义、词性、英文例句、例句中文翻译。"},
+            {"role":"system",
+             "content":"""你是一位英语老师。对每个单词给出：
+                    中文释义、词性、英文例句、例句中文翻译
+                    如果不是真实英文单词，只回复"无此单词"。"""},
             {"role":"user","content":f"请讲解单词：{user_input}"}
         ]
     )
